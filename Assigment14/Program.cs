dcb81224e6a86742e8b945d30c75d951ed7006b1
Garrett Gibbs <garrett.f.gibbs@gmail.com>
2020-03-12 10:55:25 -0700

    commit Assigment15

---
+++ Assigment14/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assigment14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string name = "Garrett";
+            //string quote = "The man said \"Hello\", Garrett. \n Hello on a new line \n \t Hello on a tab.";
+            //string fileName = @"C:\Users\Garrett";
+
+            //bool trueOrFalse = name.Contains("s");
+            //trueOrFalse = name.EndsWith("s");
+
+            //int length = name.Length;
+
+            //name = name.ToLower();
+
+            //Console.WriteLine(name);
+            //Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("My name is Garrett.");
+
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Assigment15/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+namespace Assigment15
+{
+    class Program
+    {
+        static void Main()
+        {
+            string first = "Hello, my ";
+            string second = "name is ";
+            string third = "Garrett.";
+
+            Console.WriteLine(first + second + third);
+            Console.ReadLine();
+
+            Console.WriteLine(third.ToUpper());
+            Console.ReadLine();
+
+            StringBuilder paragraph = new StringBuilder();
+
+            paragraph.Append("Hello, how are you today?");
+            paragraph.Append("\nI hope you are doing well.");
+            paragraph.Append("\nI am having a lot of fun!");
+
+            Console.WriteLine(paragraph);
+            Console.ReadLine();
+        }
+    }
+}
