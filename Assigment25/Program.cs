0aac54254deb79c1b3e4c310fc1d6e362cd15c63
Garrett Gibbs <garrett.f.gibbs@gmail.com>
2020-03-13 15:44:04 -0700

    Assigment25

---
+++ Assigment25/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assigment25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathTest test = new MathTest();
+            Console.WriteLine("Please input two whole numbers:");
+            Console.WriteLine("First number:");
+            int value1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Second number:");
+            int value2 = Convert.ToInt32(Console.ReadLine());
+            MathTest.Add(value1: value1, value2: value2);
+        }
+    }
+}
