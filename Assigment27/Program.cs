e489fde87f055447453bade75082d4fc1af07d04
Garrett Gibbs <garrett.f.gibbs@gmail.com>
2020-03-16 11:34:12 -0700

    commit Assigment27

---
+++ Assigment27/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assigment27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee example1 = new Employee() { FirstName = "Sample", LastName = "Student" };
+            example1.SayName();
+            Console.ReadLine();
+        }
+    }
+}
