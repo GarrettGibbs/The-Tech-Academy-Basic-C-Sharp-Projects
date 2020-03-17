055327331ca284d6a20334c1a9945584378bb163
Garrett Gibbs <garrett.f.gibbs@gmail.com>
2020-03-17 11:10:56 -0700

    add interface to Assigment28

--- Assigment28/Employee.cs
+++ Assigment28/Employee.cs
@@ -6,12 +6,17 @@ using System.Threading.Tasks;
 
 namespace Assigment28
 {
-    public class Employee : Person
+    public class Employee : Person, IQuittable
     {
         public override void SayName()
         {
             Console.WriteLine("The employee's name is:");
             base.SayName();
+        }
+        public void Quit()
+        {
+            Console.WriteLine("This employee has quit.");
+            Console.ReadLine();
         }
     }
 }
