dd50c1b87fbbd1633c38278d9b33b22a371137dc
Garrett Gibbs <garrett.f.gibbs@gmail.com>
2020-03-19 11:29:05 -0700

    commit Assigment32

---
+++ Assigment32/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Assigment32
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please type a number!");
+            string num = Console.ReadLine();
+            using (StreamWriter file = new StreamWriter(@"C:\Sample\numberFile.txt", true))
+            {
+                file.WriteLine(num);
+            }
+            Console.WriteLine(File.ReadAllText(@"C:\Sample\numberFile.txt"));
+            Console.ReadLine();
+        }
+    }
+}
